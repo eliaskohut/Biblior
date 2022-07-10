980154eacbe1805e0d915005a319eb582e5ef5e1
unknown <73884870+misty0mist@users.noreply.github.com>
2022-07-10 13:07:27 +0200

    initial commit

---
+++ src/main/java/com/biblior/biblior/BibliorApplication.java
@@ -0,0 +1,13 @@
+package com.biblior.biblior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/biblior/biblior/BibliorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblior.biblior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
